94b6f2fb509b8d54ecb97718567d5a1ef0b45150
Maxime Mu <mamu@msc-linux-sls-005.scss.tcd.ie>
2024-06-27 14:15:53 +0100

    fixes

--- llama3-8b/Main.py
+++ llama3-8b/Main.py
@@ -96,7 +96,7 @@ def main(
 
         # Store the results in a txt file
         print("Storing the results in txt file...")
-        lines_to_skip = ["User:", "Use", "Data:", "> Assistant:", "Note:"]
+        lines_to_skip = ["User:", "Use", "Data:", "> Assistant:", "Note:", "Patient i:", "Note that"]
 
         def filter_lines(text: str, prefixes: List[str]) -> str:
             filtered_lines = []
